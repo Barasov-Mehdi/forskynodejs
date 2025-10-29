1ad436aee82c94e5d0de0839c9770bbcd0763c0f
Mehdi <145445829+Barasov-Mehdi@users.noreply.github.com>
2025-10-29 11:06:48 -0700

    10.29.2025

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// const imageRoutes = require('./routes/imageRoutes');
+// const path = require('path');
+
+// dotenv.config();
+// const app = express();
+
+// mongoose.connect(process.env.MONGODB_URL)
+//   .then(() => console.log('MongoDB bağlantısı başarılı'))
+//   .catch(err => console.log(err));
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', imageRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Sunucu çalışıyor: http://localhost:${PORT}`));
+
+
+
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const imageRoutes = require('./routes/imageRoutes');
-const path = require('path');
-
-dotenv.config();
 const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('MongoDB bağlantısı başarılı'))
-  .catch(err => console.log(err));
+// Routes
+const imageRoutes = require('./routes/imageRoutes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -17,5 +37,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', imageRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Sunucu çalışıyor: http://localhost:${PORT}`));
+module.exports = app; // serverless uyumlu

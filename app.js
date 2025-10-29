0d2bf4a0dafd9436e6f9d7c45a3bcb9bfe727704
Mehdi <145445829+Barasov-Mehdi@users.noreply.github.com>
2025-10-29 13:56:44 -0700

    10.29.2025

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const imageRoutes = require('./routes/imageRoutes');
-const serverless = require('serverless-http'); // serverless-http kullanıyoruz
+const serverless = require('serverless-http');
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use('/api/images', imageRoutes);
 
+// MongoDB bağlantısı
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch(err => console.log('MongoDB bağlantı hatası:', err));
 
-// Vercel için default export
-module.exports = serverless(app);
+// Vercel için: serverless handler
+module.exports = app; // APP'i export et
+module.exports.handler = serverless(app); // serverless handler export

a47f22951e8658015c42b2321ccfad75391003f0
Mehdi <145445829+Barasov-Mehdi@users.noreply.github.com>
2025-10-29 12:02:33 -0700

    4

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const imageRoutes = require('./routes/imageRoutes');
 const path = require('path');
-const serverless = require('vercel-express');
+const serverless = require('vercel-express'); // veya serverless-http
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
+// Statik dosyalar (CSS, JS, img)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', imageRoutes);
 
 // MongoDB bağlantısı
@@ -20,4 +24,5 @@ mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch(err => console.log(err));
 
-module.exports = serverless(app); // <-- Serverless olarak export ediyoruz
+// Vercel için export
+module.exports = serverless(app);
